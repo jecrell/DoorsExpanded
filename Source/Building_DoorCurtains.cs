4784b716e0bbfd9a9af4831afb2457fd449aff6c
jecrell <matt.walls31@gmail.com>
2017-08-11 01:03:29 +0900

    Initial Commit

---
+++ Source/Building_DoorCurtains.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectHeron
+{
+    public class Building_DoorCurtains : Building_Door
+    {
+
+    }
+}
---
+++ Source/Building_DoorLockable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace ProjectHeron
+{
+    public class Building_DoorLockable : Building_Door
+    {
+
+    }
+}
