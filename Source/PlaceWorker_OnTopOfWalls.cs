b067ddcae8466fd1075be63d47f8c66664be7098
lbmaian <maian330@gmail.com>
2020-02-25 04:54:59 -0800

    Remove dependency on JecsTools & copy over needed classes until JecsTools migrated to 1.1

---
+++ Source/PlaceWorker_OnTopOfWalls.cs
@@ -0,0 +1,19 @@
+﻿// Note: This is copied from JecsTools.
+
+using System.Linq;
+using Verse;
+
+namespace JecsTools
+{
+    public class PlaceWorker_OnTopOfWalls : PlaceWorker
+    {
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map,
+            Thing thingToIgnore = null)
+        {
+            if (loc.GetThingList(map).FirstOrDefault(x =>
+                    x.def.defName.Contains("Wall")) != null)
+                return true;
+            return new AcceptanceReport("JT_PlaceWorker_OnTopOfWalls".Translate());
+        }
+    }
+}
