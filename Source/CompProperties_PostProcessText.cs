849f4223c0210784b10fd7bd311e8b9c9a1ac47a
lbmaian <maian330@gmail.com>
2021-08-28 22:36:27 -0700

    Revamp CompProperties_PostProcessText to fix bugs
    Fixes:
    - Blueprints and frames of our doors not having proper labels
    - CompProperties_PostProcessText not being removed after processing if not in dev mode
    
    Also:
    - Related debug logging
    - TLog support for static methods

--- Source/CompProperties_PostProcessText.cs
+++ Source/CompProperties_PostProcessText.cs
@@ -14,8 +14,16 @@ namespace DoorsExpanded
         public ThingDef defaultLabelAndDescriptionFrom;
         public bool appendSizeToLabel;
 
-        [Unsaved(false)]
-        private string origLabel;
+        [Unsaved]
+        public string origLabel;
+
+        [Unsaved]
+        public string baseLabel;
+
+        [Unsaved]
+        private bool finalized = false;
+
+        public static readonly Dictionary<ThingDef, CompProperties_PostProcessText> defToComp = new();
 
         public CompProperties_PostProcessText()
         {
@@ -24,30 +32,77 @@ namespace DoorsExpanded
 
         public override void ResolveReferences(ThingDef parentDef)
         {
-            // Note: ResolveReferences can run more than once for a single def, so ensure it's idempotent.
+            // This method can be called multiple times per instance, so ensure idempotency.
+            if (finalized)
+                return;
+
+            defToComp[parentDef] = this;
+
+            origLabel = parentDef.label;
+            // This logic can't be done in PostLoadSpecial due to needing defaultLabelAndDescriptionFrom needing to be resolved.
             if (defaultLabelAndDescriptionFrom is not null)
             {
+                var otherPostProcessText = defaultLabelAndDescriptionFrom.GetCompProperties<CompProperties_PostProcessText>();
+                otherPostProcessText?.ResolveReferences(defaultLabelAndDescriptionFrom);
                 if (parentDef.label.NullOrEmpty())
-                    parentDef.label =
-                        defaultLabelAndDescriptionFrom.GetCompProperties<CompProperties_PostProcessText>()?.origLabel ??
-                        defaultLabelAndDescriptionFrom.label;
+                {
+                    var newLabel = otherPostProcessText?.baseLabel ?? defaultLabelAndDescriptionFrom.label;
+                    if (TLog.Enabled)
+                        TLog.Log(this, $"{parentDef}.label: {QuoteString(parentDef.label)} => {QuoteString(newLabel)}");
+                    parentDef.label = newLabel;
+                }
                 if (parentDef.description.NullOrEmpty())
-                    parentDef.description = defaultLabelAndDescriptionFrom.description;
+                {
+                    var newDescription = defaultLabelAndDescriptionFrom.description;
+                    if (TLog.Enabled)
+                        TLog.Log(this, $"{parentDef}.description: {QuoteString(parentDef.description)} => {QuoteString(newDescription)}");
+                    parentDef.description = newDescription;
+                }
             }
-            if (appendSizeToLabel && origLabel is null)
+            baseLabel = parentDef.label;
+            if (appendSizeToLabel)
             {
-                origLabel = parentDef.label;
                 var size = parentDef.Size;
-                parentDef.label = $"{parentDef.label} ({size.x}x{size.z})";
+                var newLabel = $"{parentDef.label} ({size.x}x{size.z})";
+                if (TLog.Enabled)
+                    TLog.Log(this, $"{parentDef}.label: {QuoteString(parentDef.label)} => {QuoteString(newLabel)}");
+                parentDef.label = newLabel;
             }
+            finalized = true;
         }
 
-        // This serves as a useful hook for cleaning up after ourselves.
-        public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
+        internal static string QuoteString(string str) => str is null ? "null" : $"\"{str.Replace("\\", "\\\\").Replace("\"", "\\\"")}\"";
+    }
+
+    [StaticConstructorOnStartup]
+    public static class PostProcessTextOnStartup
+    {
+        static PostProcessTextOnStartup()
         {
-            // Comps add overhead, so since we're done, remove ourselves.
-            parentDef.comps.Remove(this);
-            yield break;
+            // Update frames and blueprints that were implicitly generated for defs with CompProperties_PostProcessText.
+            // These are generated before ResolveReferences are ever called.
+            foreach (var def in DefDatabase<ThingDef>.AllDefsListForReading)
+            {
+                if (def.entityDefToBuild is ThingDef entityDef &&
+                    CompProperties_PostProcessText.defToComp.TryGetValue(entityDef, out var postProcessTextComp))
+                {
+                    var newLabel = entityDef.label +
+                        (postProcessTextComp.origLabel.NullOrEmpty() ? def.label : def.label.Substring(postProcessTextComp.origLabel.Length));
+                    if (TLog.Enabled)
+                        TLog.Log(typeof(PostProcessTextOnStartup), string.Format("{0}.label: {1} => {2}",
+                            def, CompProperties_PostProcessText.QuoteString(def.label), CompProperties_PostProcessText.QuoteString(newLabel)));
+                    def.label = newLabel;
+                }
+            }
+
+            // Comps add overhead, so since we're done, remove the comps now that they're no longer needed.
+            foreach (var (def, postProcessTextComp) in CompProperties_PostProcessText.defToComp)
+            {
+                if (TLog.Enabled)
+                    TLog.Log(typeof(PostProcessTextOnStartup), $"Removing {postProcessTextComp} from {def}");
+                def.comps.Remove(postProcessTextComp);
+            }
+            CompProperties_PostProcessText.defToComp.Clear();
         }
     }
 }
